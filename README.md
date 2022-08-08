78838232eef6458b36d2067676b6c3998e52d531
raylax <raylax@inurl.org>
2022-08-08 10:47:04 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # jet-hack
 
-## DDD
+根据`ja-netfilter`源代码简化
+> 仅测试了**2020.2**版本
+> 
+> 请将`jar`文件放在不会删除的路径，否则删除后将无法启动IDE
+
+`Mac OS`可以直接`java -jar jet-hack-x.x.jar`根据提示自动配置
+
+其他操作系统可以修改`xxx.vmoptions`添加`-javaagent:/path/to/jet-hack-x.x.jar`
+
+然后注册选择`Activation code`，访问以下网址填入对应的key，点击`Activate`
+
+https://jetbra.in/s
--- src/main/java/org/inurl/jethack/Jethack.java
+++ src/main/java/org/inurl/jethack/Jethack.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 import java.util.jar.JarFile;
 
 public class Jethack {
-    public static final String VERSION = "1.0-preview";
+    public static final String VERSION = "1.2";
     private static final String FLAG_FILE = "/where_is_jar.flag";
     public static String SELF_PATH;
     private static final String CONFIG_MARK = "### JET-HACK ###";
